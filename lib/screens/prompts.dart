88fb815ed720be72159b1f2f6df233a8322de6f5
sammi <txl.sammi@gmail.com>
2024-08-15 17:53:21 +1000

    added vscode settings to .gitignore

--- lib/screens/prompts.dart
+++ lib/screens/prompts.dart
@@ -1,8 +1,6 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:modular_ui/modular_ui.dart';
 
 class PromptsPage extends StatelessWidget {
   
